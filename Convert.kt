436697755cbbf1bff17ec2a4ecc8ff68aeceb36b
unknown <jenilparmar1999@gmail.com>
2023-02-04 02:08:23 +0530

    get value from user that would be converted into another unit.

--- Convert.kt
+++ Convert.kt
@@ -1,3 +1,11 @@
 // Name : Jenil Parmar
 // student id : A00271994
 // course name : mobile application development
+
+
+fun main(){
+
+	// Get value from user
+	print("\nEnter a number that you want to convert it in a different unit: ")
+	val enteredNumber: Double = readLine()!!.toDouble()
+}
